ed5e81f8d15df08e4f389526be87a55b4bbf76d0
Franco Dev <gfrancojf@gmail.com>
2022-10-17 15:12:37 -0400

    base 2

--- sgv/admin.py
+++ sgv/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from . models import Empresa, Cargo, Departaments
+
 
 # Register your models here.
+@admin.register(Empresa)
+class GestionEmpresa(admin.ModelAdmin):
+   list_display = ( 'id','nEmbresa', 'clase_rif', 'rif','naCronimo','logoImg',)
+   list_display_links = ('nEmbresa',)
+
+
+@admin.register(Departaments)
+class GestionDepartaments(admin.ModelAdmin):
+   list_display = ( 'id','Nempresa','nOficina','extTelefono',)
+   list_display_links = ('nOficina',)
+
+
+@admin.register(Cargo)
+class GestionCargo(admin.ModelAdmin):
+    list_display = ('id',)
--- sgv/models.py
+++ sgv/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from phonenumber_field.modelfields import PhoneNumberField
 # Create your models here.
 
 
@@ -12,3 +12,43 @@ class BaseModel(models.Model):
  
     class Meta:
         abstract = True
+class Empresa(BaseModel):
+    nEmbresa = models.CharField('Razon Social', max_length=60, unique=True, null=False, blank=False)
+    tiporif = [('V', 'Natural de la República Bolivariana de Venezuela'),
+               ('J', 'Personal Jurídica'),
+               ('G', 'Ente Gubernamental'),
+               ('E','Extranjero con residencia en Venezuela'),
+               ('P', 'Agente Registrado Con Pasaporte'),                   
+               ]
+    clase_rif = models.CharField('Estatus del Empleado', max_length=1, choices=tiporif, null=False, default='G')
+    rif = models.CharField('RIF', max_length=10, unique=True, null=False, blank=False)
+    naCronimo = models.CharField('Acronimo', max_length=60, unique=True, null=True, blank=True)
+    logoImg = models.ImageField('LOGOTIPO', null=True, blank=True, upload_to='empresa/logo')
+    class Meta:
+        verbose_name = 'Empresa'
+        verbose_name_plural = 'Empresas'
+    def __str__(self) -> str:
+        return self.nEmbresa 
+
+class Departaments(BaseModel):
+    Nempresa = models.ForeignKey(Empresa,verbose_name="Empresa" ,on_delete=models.CASCADE)
+    nOficina = models.CharField('Oficina a Visitar', max_length=30, unique=True, null=True, blank=True)
+    extTelefono =  PhoneNumberField(blank=True, help_text='Contact phone number')
+    class Meta:
+        verbose_name = 'Departamento'
+        verbose_name_plural = 'Departamentos '
+
+    def __str__(self) -> str:
+        return self.nOficina 
+
+class Cargo(BaseModel):
+    nDepartamentos = models.ForeignKey(Departaments, on_delete=models.CASCADE)
+    nCargo = models.CharField('Cargo', max_length=50)
+
+    class Meta:
+        verbose_name = 'Cargo'
+        verbose_name_plural = 'Cargos'
+    
+    def __str__(self):
+        return self.nCargo
+    
